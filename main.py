fff9bd325b64d36711e6e4a8e8f2f62ee136ce48
JustCallMeZack <Zletts100@gmail.com>
2025-05-09 20:16:32 -0400

    Updated bot to include sorted and formatted dictionary. added formatting for the final print to be more readable

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from stats import get_book_text
 from stats import word_count
-
+from stats import char_count
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+    book_text = get_book_text(path)
     num_words = word_count(book_text)
-    print(f"{num_words} words found in the document")
+    chars = char_count(path)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in chars:
+        char = i["char"]
+        count = i["num"]
+        if char == "\n":
+            char_display = "\\n"
+        else:
+            char_display = char
+        print(f"{char_display}: {count}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -6,3 +6,33 @@ def get_book_text(path):
 def word_count(book_string):
     word_list = book_string.split()
     return len(word_list)
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+
+def char_count(path):
+    chars = get_book_text(path)
+    char_count = {}
+#   count chars
+
+    for char in chars:
+        if char.lower() in char_count:
+            char_count[char.lower()] += 1
+        elif char.lower() not in char_count:
+            char_count[char.lower()] = 1
+    char_count_pair = []
+
+#   sort chars
+
+    for char, count in char_count.items():
+        char_count_pair.append({"char": char, "num": count})
+
+    char_count_pair.sort(reverse=True, key=sort_on)
+
+    return char_count_pair
+
+
+    
