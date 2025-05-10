8f47f481f8afd333d67f21d49fac704555f2cc38
JustCallMeZack <Zletts100@gmail.com>
2025-05-09 20:27:33 -0400

    Added argument functionaility to specify which book to look at

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import sys
 from stats import get_book_text
 from stats import word_count
 from stats import char_count
 
+
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    
+    path = sys.argv[1]
     book_text = get_book_text(path)
     num_words = word_count(book_text)
     chars = char_count(path)
